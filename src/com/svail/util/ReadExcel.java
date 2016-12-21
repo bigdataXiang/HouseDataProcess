72aacc928ea6e06da9f9f9ed2e71f72ec902bf18
郁金香的仰望 <466194593@qq.com>
2016-12-21 19:00:14 +0800

    ~~

--- src/com/svail/util/ReadExcel.java
+++ src/com/svail/util/ReadExcel.java
@@ -1,5 +1,8 @@
 package com.svail.util;
 
+import net.sf.json.JSONObject;
+import org.apache.commons.collections.Buffer;
+import org.apache.poi.POIXMLException;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,19 +11,32 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
 
 /**
  * Created by ZhouXiang on 2016/12/21.
+ *
+ * 内存溢出：-Xms258m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=768m
+ *
+ * //市界的地理坐标为：北纬39”26’至41”03’，东经115”25’至 117”30’。
  */
 public class ReadExcel {
     public static void main(String[] args){
-        readByLine("D:\\P02机场_point.xlsx");
+        Vector<String> files=FileTool.Load("D:\\小论文\\poi资料\\shpPoi\\filename.txt","utf-8");
+        for(int i=0;i<files.size();i++){
+            System.out.println(i+":"+files.elementAt(i));
+            readExcel(files.elementAt(i));
+        }
+        //readExcel("D:\\小论文\\poi资料\\北京poi\\北京市.xlsx");
+        //D:\小论文\poi资料\北京poi
+        //D:\小论文\poi资料\shpPoi数据\
+
     }
-    public static void readByLine(String fileName){
+    public static void readExcel(String fileName){
         boolean isE2007 = false;    //判断是否是excel2007格式
         if(fileName.endsWith("xlsx"))
             isE2007 = true;
@@ -34,35 +50,96 @@ public class ReadExcel {
                 wb = new HSSFWorkbook(input);
             Sheet sheet = wb.getSheetAt(0);     //获得第一个表单
             Iterator<Row> rows = sheet.rowIterator(); //获得第一个表单的迭代器
+            int row_count=0;
+            Map<Integer,String> names=new HashMap<>();
+
+            JSONObject obj=new JSONObject();
+            Cell cell_value;
             while (rows.hasNext()) {
+
                 Row row = rows.next();  //获得行数据
-                System.out.println("Row #" + row.getRowNum());  //获得行号从0开始
-                Iterator<Cell> cells = row.cellIterator();    //获得第一行的迭代器
-                while (cells.hasNext()) {
-                    Cell cell = cells.next();
-                    //System.out.print(" "+cell.getColumnIndex());
-                    switch (cell.getCellType()) {   //根据cell中的类型来输出数据
-                        case HSSFCell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue()+" ");
-                            break;
-                        case HSSFCell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue()+" ");
-                            break;
-                        case HSSFCell.CELL_TYPE_BOOLEAN:
-                            System.out.print(cell.getBooleanCellValue()+" ");
-                            break;
-                        case HSSFCell.CELL_TYPE_FORMULA:
-                            System.out.print(cell.getCellFormula()+" ");
-                            break;
-                        default:
-                            System.out.print("unsuported sell type");
-                            break;
+                row_count=row.getRowNum();//获得行号从0开始
+
+                if(row_count==0){
+                    Iterator<Cell> cells = row.cellIterator();    //获得第一行的迭代器
+                    int col_count=0;
+                    while (cells.hasNext()) {
+                        Cell cell = cells.next();
+                        names.put(col_count,cell.getStringCellValue());
+                        col_count++;
                     }
+                }else {
+                    for(int i=0;i<names.size();i++){
+                        cell_value=row.getCell(i);
+                        if(cell_value!=null){
+                            obj.put(names.get(i),cell_value.toString());
+                        }
+                    }
+                    //System.out.println(obj);
+                    FileTool.Dump(obj.toString(),fileName.replace(".xlsx","").replace(".xls","")+"_json.txt","utf-8");
                 }
-                System.out.println("\n");
             }
         } catch (IOException ex) {
             ex.printStackTrace();
+        }catch (POIXMLException e){
+            e.getStackTrace();
         }
+    }
+
+    //将csv文件转换成txt文件再进行读写操作
+    public static void readCSV(String file,String[] names){
+        try {
+            BufferedReader br=new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            int count=0;
+            String[] infos;
+
+            for(String line=br.readLine();line!=null;line=br.readLine()){
+                count++;
+                System.out.println(count);
+                infos=line.split(",");
+
+                if(names.length==infos.length){
+                    JSONObject obj=new JSONObject();
+                    for(int i=0;i<names.length;i++){
+                        obj.put(names[i],infos[i]);
+                    }
+                    FileTool.Dump(obj.toString(),file.replace(".txt","_json.txt"),"utf-8");
+                }else {
+                    System.out.println("数组长度不一致");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static JSONObject readByRow(Cell cell){
+        JSONObject obj=new JSONObject();
+        int count=0;
+            count++;
+            switch (cell.getCellType()) {   //根据cell中的类型来输出数据
+                case HSSFCell.CELL_TYPE_NUMERIC:
+                    obj.put("" + count, cell.getNumericCellValue());
+                    break;
+                case HSSFCell.CELL_TYPE_STRING:
+                    obj.put("" + count, cell.getStringCellValue());
+                    break;
+                case HSSFCell.CELL_TYPE_BOOLEAN:
+                    obj.put("" + count, cell.getBooleanCellValue());
+                    break;
+                case HSSFCell.CELL_TYPE_FORMULA:
+                    obj.put("" + count, cell.getCellFormula());
+                    break;
+                case HSSFCell.CELL_TYPE_BLANK:
+                    System.out.println("空白");
+                    break;
+                default:
+                    System.out.print("unsuported sell type");
+                    break;
+            }
+        return obj;
     }
 }
---
+++ src/com/svail/util/getNamesInFolder.java
@@ -0,0 +1,45 @@
+package com.svail.util;
+
+import java.io.File;
+
+/**
+ * Created by ZhouXiang on 2016/12/21.
+ */
+public class getNamesInFolder {
+    public static void main(String[] args) {
+
+        String path = "D:\\小论文\\poi资料\\shpPoi\\";
+        //getFileName(path);
+        getFilePath(path);
+    }
+
+    private static void getFileName(String path){
+        File file = new File(path);
+        File[] array = file.listFiles();
+
+        for(int i=0;i<array.length;i++){
+            if(array[i].isFile()){
+                System.out.println(array[i].getName());
+                FileTool.Dump(array[i].getName(),path+"filename.txt","utf-8");
+            }else if(array[i].isDirectory()){
+                getFileName(array[i].getPath());
+            }
+        }
+    }
+
+    private static void getFilePath(String path){
+        File file = new File(path);
+        File[] array = file.listFiles();
+
+        for(int i=0;i<array.length;i++){
+            if(array[i].isFile()){
+                System.out.println(array[i].getPath());
+                FileTool.Dump(array[i].getPath(),path+"filename.txt","utf-8");
+            }else if(array[i].isDirectory()){
+                getFilePath(array[i].getPath());
+            }
+        }
+    }
+
+
+}
